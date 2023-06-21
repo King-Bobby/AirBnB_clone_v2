da8c921cc66f07486ba8e95d9f679e69614106c8
Sir-Blow <emmanuelakej@gmail.com>
2023-06-21 09:17:29 -0700

    Test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -26,7 +26,7 @@ class testConsole(unittest.TestCase):
         self.assertTrue(len(console.__doc__) >= 1,
                         "console.py needs a docstring")
 
-    def test_pep8_conformity_of_the_console(self):
+    def test_pep8_conformance_test_console(self):
         """Test that tests/test_console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['tests/test_console.py'])
@@ -46,7 +46,7 @@ class testConsole(unittest.TestCase):
         self.assertIsNotNone(HBNBCommand.do_all.__doc__)
         self.assertIsNotNone(HBNBCommand.do_update.__doc__)
         self.assertIsNotNone(HBNBCommand.default.__doc__)
-    
+
      def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
         self.assertIsNot(HBNBCommand.__doc__, None,
