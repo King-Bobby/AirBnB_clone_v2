4ed503e80313096c5837fdf6cf828761feefb052
Sir-Blow <emmanuelakej@gmail.com>
2023-06-21 05:53:46 -0700

    updated file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,10 +2,6 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
-classes = {"BaseModel": BaseModel, "User": User, "State": State, "City": City,
-           "Amenity": Amenity, "Place": Place, "Review": Review}
-
-
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
@@ -16,13 +12,7 @@ class FileStorage:
         return FileStorage.__objects
         if not cls:
             return FileStorage.__objects
-        if cls in classes.values():
-            cpy = {}
-            for k, v in FileStorage.__objects.items():
-                if isinstance(v, cls):
-                    cpy[k] = FileStorage.__objects[k]
-            return cpy
-
+        
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
