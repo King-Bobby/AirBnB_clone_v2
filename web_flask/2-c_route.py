d6dc4b3b28fe49fb627f9072b1e1b7a715c94756
King-Bobby <noelbrowndboss@gmail.com>
2023-07-24 01:51:54 +0000

    c text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Starts flask web application listening on 0.0.0.0, port 5000
+Routes:
+/: display Hello HBNB!
+/hbnb: display HBNB
+/c/<text>: display "C" followed by the value of the text variable
+	(replace underscore _ symbols with a space)
+"""
+
+
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+	"""displays Hello HBNB"""
+	return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+	"""displays HBNB"""
+	return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+	"""jj"""
+	return "C {}".format(escape(text.replace('_', ' ')))
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000) 
